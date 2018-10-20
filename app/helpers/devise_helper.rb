cbe30fd6b14a6c865ab7c7219a68866413994c14
Matthew Nguyen <contact@n-studio.fr>
2018-10-20 17:48:03 +0200

    reformat helper file

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,9 +3,7 @@ module DeviseHelper
     return "" unless devise_error_messages?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
+    sentence = I18n.t("errors.messages.not_saved", count: resource.errors.count, resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div class="notification">
@@ -20,5 +18,4 @@ module DeviseHelper
   def devise_error_messages?
     !resource.errors.empty?
   end
-
 end
